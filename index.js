98823a6334f098a92e51179d90c11ef6f0e01e0d
YangHanGil <hangil968@gmail.com>
2020-02-05 09:08:34 +0900

    second commit

--- index.js
+++ index.js
@@ -1,14 +1,15 @@
+/*
 var express = require('express');
 var app = express();
 
-app.set('view engine','ejs');
+app.set('view engine','ejs'); // 1
 app.use(express.static(__dirname + '/public'));
 
-app.get('/hello', function(req,res){
+app.get('/hello', function(req,res){ // 2
   res.render('hello', {name:req.query.nameQuery});
 });
 
-app.get('/hello/:nameParam', function(req,res){
+app.get('/hello/:nameParam', function(req,res){ // 3
   res.render('hello', {name:req.params.nameParam});
 });
 
@@ -16,3 +17,23 @@ var port = 3000;
 app.listen(port, function(){
   console.log('server on! http://localhost:'+port);
 });
+*/
+var mysql      = require('mysql');
+var connection = mysql.createConnection({
+  host     : 'localhost',
+  user     : 'hangil',
+  password : 'hangil',
+  port     : 3306,
+  database : 'test_1'
+});
+
+connection.connect();
+
+connection.query('SELECT * from Persons', function(err, rows, fields) {
+  if (!err)
+    console.log('The solution is: ', rows);
+  else
+    console.log('Error while performing Query.', err);
+});
+
+connection.end();
